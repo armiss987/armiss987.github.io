82d00efd090e07b2e30d368d0be23e9b7834336b
armiss987 <42564518+armiss987@users.noreply.github.com>
2018-08-21 15:00:32 +0600

    6

--- mail.php
+++ mail.php
@@ -10,3 +10,5 @@ $message = "Имя: $name \nТелефон: $phone \nТекст: $text";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
