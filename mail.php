d4e27b061856ec7dcc3be4665bfdedd67770cdd2
armiss987 <42564518+armiss987@users.noreply.github.com>
2018-08-21 15:37:52 +0600

    8976

--- mail.php
+++ mail.php
@@ -1,14 +1,40 @@
-<?php
+<?php 
 
-$recepient = "sertish25@yandex.ru";
-$sitename = "Текстория.рф";
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
 
-$name = trim($_POST["name"]);
-$email = trim($_POST["email"]);
-$text = trim($_POST["text"]);
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$text = $_POST['text'];
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'sertish24@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'artm020202'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('sertish24@yandex.ru'); // от кого будет уходить письмо?
+$mail->addAddress('sertish25@yandex.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с сайта';
+$mail->Body    = '' .$name . ' оставил заявку, его почта ' .$email. '<br>Сообщение этого пользователя: ' .$text;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: more/more.html');
+}
 ?>
